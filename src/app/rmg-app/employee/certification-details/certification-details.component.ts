11db058fa3dfafcdfa1995893ab1c4c449d61ed9
Shanker Kalluri <E003801@cigniti.com>
2017-12-15 16:20:55 +0530

    validation for certification details

--- src/app/rmg-app/employee/certification-details/certification-details.component.ts
+++ src/app/rmg-app/employee/certification-details/certification-details.component.ts
@@ -14,22 +14,41 @@ export class CertificationDetailsComponent implements OnInit {
                               {label:'certification3', value:'certification3'}];
 
     public levelDetails = [{label:'level1', value:'levlel1'},
-                           {label:'level1', value:'levlel1'},
-                           {label:'level1', value:'levlel1'}];   
+                           {label:'level2', value:'levlel2'},
+                           {label:'level3', value:'levlel3'}];   
 
    public certificationHeader:any = [
-    {field: 'certificationName', header: 'Certification Name'},
+      {field: 'certificationName', header: 'Certification Name'},
       {field: 'certificationForm', header: 'Certification From'},
       {field: 'validFrom', header: 'Valid From'},
       {field: 'validTo', header: 'Valid To'},
       {field: 'comments', header: 'Comments'}
   ];
+  public certificationModel: any = {
+    certificationName: '',
+    certificationLevel:'levlel1',
+
+  };
   
   constructor() { }
 
   ngOnInit() {
     console.log(this.certificationDetails)
      
+  }
+
+  validateFields(){
+    let isValid = true;
+    ['certificationName', 'certificationFrom', 'validFrom', 'validTo'].forEach((value) => {
+      if(!this.certificationModel[value]){
+        isValid = false;
+      }
+    })
+    if(!isValid){
+      return true
+    }else{
+      return false;
+    }
   }
   
 }
